5030d1d8f869c347b4adef0cc4ea8df9951a3a68
Vincent <v.ketteniss@outlook.com>
2025-06-04 12:39:44 +0200

    refactor: improving readablility

--- module_09/ex00/BitcoinExchange.cpp
+++ module_09/ex00/BitcoinExchange.cpp
@@ -37,12 +37,6 @@ std::deque<std::deque<std::string> > BitcoinExchange::readDb(const std::string &
 	return (dbParsed);
 }
 
-bool BitcoinExchange::isValidDate(const std::string &date) const
-{
-	Date dbDate(date);
-	return dbDate.validDate;
-}
-
 bool BitcoinExchange::isValidValue(const std::string &value) const
 {
 	char *endPtr = NULL;
@@ -97,61 +91,153 @@ BitcoinExchange::BitcoinExchange(const std::string &accountDb)
 
 void BitcoinExchange::printHistory(void)
 {
-	for (std::deque<std::deque<std::string> >::const_iterator accountRow = _accountData.begin(); accountRow != _accountData.end(); ++accountRow)
+	std::deque<std::deque<std::string> >::const_iterator accountRow = _accountData.begin();
+	for (; accountRow != _accountData.end(); ++accountRow)
 	{
-		const std::string & dateAccountRow = (*accountRow).at(0);
-		const std::string & valueAccountRow = (*accountRow).at(1);
-		if (!isValidDate(dateAccountRow) || !isValidValue(valueAccountRow))
+		const std::string & accountDateRaw = (*accountRow).at(0);
+		const std::string & accountValueRaw = (*accountRow).at(1);
+		if (!validDateFormat(accountDateRaw) || !isValidValue(accountValueRaw))
 			continue;
-
-		Date dateAccount(dateAccountRow);
-		for (std::deque<std::deque<std::string> >::const_iterator exchangeRateRow = _exchangeRates.begin(); exchangeRateRow != _exchangeRates.end(); ++exchangeRateRow)
+		Date date(accountDateRaw);
+		std::deque<std::deque<std::string> >::const_iterator rateRow = _exchangeRates.begin();
+		for (; rateRow != _exchangeRates.end(); ++rateRow)
 		{
-			const std::string & dateExchangeRateStr = (*exchangeRateRow).at(0);
-			const std::string & valueExchangeRateStr = (*exchangeRateRow).at(1);
-			Date dateExchangeRate(dateExchangeRateStr);
-			if (!isValidDate(dateExchangeRateStr) || !isValidValue(valueExchangeRateStr))
+			const std::string & rateDateRaw = (*rateRow).at(0);
+			const std::string & rateValueRaw = (*rateRow).at(1);
+			Date dateExchangeRate(rateDateRaw);
+			if (!validDateFormat(rateDateRaw) || !isValidValue(rateValueRaw))
 				continue ;	
-			float valueAccount = strtof(valueAccountRow.c_str(), NULL);
-			if (dateExchangeRate == dateAccount)
+			float accountValue = strtof(accountValueRaw.c_str(), NULL);
+			if (dateExchangeRate == date)
 			{
-				float valueExchangeRate = strtof(valueExchangeRateStr.c_str(), NULL);
-				std::cout << dateAccountRow << " => " << valueAccount << " * " << valueExchangeRate << " = " << valueAccount * valueExchangeRate << std::endl;
+				float rateValue = strtof(rateValueRaw.c_str(), NULL);
+				std::cout << accountDateRaw << " => " << accountValue << " * " << rateValue << " = " << accountValue * rateValue << std::endl;
 				break;
 			}
-			else if (dateExchangeRate > dateAccount && exchangeRateRow != _exchangeRates.begin())
+			else if (dateExchangeRate > date && rateRow != _exchangeRates.begin())
 			{
 
-				float valueExchangeRate = strtof((*(exchangeRateRow - 1)).at(1).c_str(), NULL);
-				std::cout << dateAccountRow << " => " << valueAccount << " * " << valueExchangeRate << " = " << valueAccount * valueExchangeRate << std::endl;
+				float rateValue = strtof((*(rateRow - 1)).at(1).c_str(), NULL);
+				std::cout << accountDateRaw << " => " << accountValue << " * " << rateValue << " = " << accountValue * rateValue << std::endl;
 				break;
 			}
 
-/* 			const std::string & dateExchangeRateStr = (*exchangeRateRow).at(0);
-			const std::string & valueExchangeRateStr = (*exchangeRateRow).at(1);
-			Date dateExchangeRate((*exchangeRateRow).at(0));
-			if (!isValidDate(dateExchangeRateStr) || !isValidValue(valueExchangeRateStr))
+/* 			const std::string & rateDateRaw = (*rateRow).at(0);
+			const std::string & rateValueRaw = (*rateRow).at(1);
+			Date dateExchangeRate((*rateRow).at(0));
+			if (!validDateFormat(rateDateRaw) || !isValidValue(rateValueRaw))
 				continue ;	
-			if (dateExchangeRate < dateAccount)
+			if (dateExchangeRate < date)
 				continue ;
-			else if (dateExchangeRate == dateAccount)
+			else if (dateExchangeRate == date)
 			{
-				float valueAccount = strtof(valueAccountRow.c_str(), NULL);
-				float valueExchangeRate = strtof(valueExchangeRateStr.c_str(), NULL);
+				float accountValue = strtof(accountValueRaw.c_str(), NULL);
+				float rateValue = strtof(rateValueRaw.c_str(), NULL);
 			}
-			else if (exchangeRateRow != _exchangeRates.begin())
+			else if (rateRow != _exchangeRates.begin())
 			{
-				float valueAccount = strtof((*(exchangeRateRow - 1)).at(0).c_str(), NULL);
-				float valueExchangeRate = strtof((*(exchangeRateRow - 1)).at(1).c_str(), NULL);
+				float accountValue = strtof((*(rateRow - 1)).at(0).c_str(), NULL);
+				float rateValue = strtof((*(rateRow - 1)).at(1).c_str(), NULL);
 			}
 			else 
 				continue; */
 
-			// std::cout << dateAccountRow << " => " << valueAccountRow << " = " << valueAccount * valueExchangeRate << std::endl;
+			// std::cout << accountDateRaw << " => " << accountValueRaw << " = " << accountValue * rateValue << std::endl;
 		}
 	}
 }
 
 BitcoinExchange::~BitcoinExchange()
 {
+}
+
+struct BitcoinExchange::Date 
+{
+	int year;
+	int month;
+	int day;
+	bool validDate;
+
+	Date(const std::string& dateStr); 
+
+	bool operator<(const Date& other) const; 
+	bool operator==(const Date& other) const; 
+	bool operator>(const Date& other) const; 
+	bool operator>=(const Date& other) const; 
+	bool operator<=(const Date& other) const; 
+};
+
+BitcoinExchange::Date::Date(const std::string& dateStr)
+{
+	if (validDateFormat(dateStr))
+	{
+		year = atoi(dateStr.substr(0, 4).c_str());
+		month = atoi(dateStr.substr(5, 2).c_str());
+		day = atoi(dateStr.substr(8, 2).c_str());
+
+		int daysInMonth;
+		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
+			daysInMonth = 28;
+		else if (month == 2)
+			daysInMonth = 29;
+		else if (month == 4 || month == 6 || month == 9 || month == 11)
+			daysInMonth = 30;
+		else 
+			daysInMonth = 31;
+		
+		if (day <= daysInMonth && month <= 12 && year > 2008)
+		{
+			std::time_t current_time = std::time(0);
+			std::tm* local_time = std::localtime(&current_time);
+			int currentDay = local_time->tm_mday;
+			int currentMonth = local_time->tm_mon + 1;
+			int currentYear = local_time->tm_year + 1900;
+
+			if (year != currentYear) validDate = year < currentYear;
+			else if (month != currentMonth) validDate =  month < currentMonth;
+			else validDate =  day < currentDay;
+		}
+		else
+			validDate = false;
+	}
+}
+
+bool BitcoinExchange::Date::operator<(const Date& other) const
+{
+	if (year != other.year) return year < other.year;
+	if (month != other.month) return month < other.month;
+	return day < other.day;
+}
+
+bool BitcoinExchange::Date::operator==(const Date& other) const
+{
+	return year == other.year && month == other.month && day == other.day;
+}
+
+bool BitcoinExchange::Date::operator>(const Date& other) const
+{
+	return !(*this < other || *this == other);
+}
+
+bool BitcoinExchange::Date::operator>=(const Date& other) const
+{
+	return (*this > other || *this == other);
+}
+
+bool BitcoinExchange::Date::operator<=(const Date& other) const
+{
+	return (*this < other || *this == other);
+}
+
+bool BitcoinExchange::validDateFormat(const std::string & accountDateRaw) const
+{
+	for (unsigned int i = 0; i < accountDateRaw.size(); ++i)
+	{
+		char currentChar = accountDateRaw.c_str()[i];
+		if ((i == 4 || i == 7) && currentChar != '-')
+			return false;
+		else if (!std::isdigit(currentChar))
+			return false;
+	}
+	return true;
 }
--- module_09/ex00/BitcoinExchange.hpp
+++ module_09/ex00/BitcoinExchange.hpp
@@ -32,82 +32,12 @@ class BitcoinExchange
 		std::deque<std::deque<std::string> >	_exchangeRates;
 
 		std::deque<std::deque<std::string> >	readDb(const std::string & db, const char sep);
-		bool isValidDate(const std::string & date) const;
+		bool validDateFormat(const std::string & date) const;
 		bool isValidValue(const std::string & value) const;
 
-		struct Date
-		{
-			int year;
-			int month;
-			int day;
-			bool validDate;
+		struct Date;
 
-			Date(const std::string& dateStr) {
-				validDate = true;
-				for (unsigned int i = 0; i < dateStr.size(); ++i)
-				{
-					if (i != 4 && i != 7)
-					{
-						if (!isdigit(dateStr.c_str()[i]))
-							validDate = false;
-					}
-					else if (dateStr.c_str()[i] != '-') validDate = false;
-				}
-				if (validDate)
-				{
-					year = atoi(dateStr.substr(0, 4).c_str());
-					month = atoi(dateStr.substr(5, 2).c_str());
-					day = atoi(dateStr.substr(8, 2).c_str());
 
-					int daysInMonth;
-					if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
-						daysInMonth = 28;
-					else if (month == 2)
-						daysInMonth = 29;
-					else if (month == 4 || month == 6 || month == 9 || month == 11)
-						daysInMonth = 30;
-					else 
-						daysInMonth = 31;
-					
-					if (day <= daysInMonth && month <= 12 && year > 2008)
-					{
-						std::time_t current_time = std::time(0);
-						std::tm* local_time = std::localtime(&current_time);
-						int currentDay = local_time->tm_mday;
-						int currentMonth = local_time->tm_mon + 1;
-						int currentYear = local_time->tm_year + 1900;
-
-						if (year != currentYear) validDate = year < currentYear;
-						else if (month != currentMonth) validDate =  month < currentMonth;
-						else validDate =  day < currentDay;
-					}
-					else
-						validDate = false;
-				}
-			}
-
-			bool operator<(const Date& other) const {
-				if (year != other.year) return year < other.year;
-				if (month != other.month) return month < other.month;
-				return day < other.day;
-			}
-
-			bool operator==(const Date& other) const {
-				return year == other.year && month == other.month && day == other.day;
-			}
-
-			bool operator>(const Date& other) const {
-				return !(*this < other || *this == other);
-			}
-
-			bool operator>=(const Date& other) const {
-				return (*this > other || *this == other);
-			}
-
-			bool operator<=(const Date& other) const {
-				return (*this < other || *this == other);
-			}
-		};
 };
 
 #endif
